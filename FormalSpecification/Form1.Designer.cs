5067afdb62fd487ca56acfb57ab81857d15c82cb
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2022-10-04 14:55:49 +0700

    Add project files.

---
+++ FormalSpecification/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace FormalSpecification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnOpenFile = new System.Windows.Forms.ToolStripButton();
+            this.btnSaveFile = new System.Windows.Forms.ToolStripButton();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnConvertToCSharp = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnConvertToCplusplus = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.outputTextbox = new System.Windows.Forms.RichTextBox();
+            this.inputTextbox = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.TbFileName = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.GripMargin = new System.Windows.Forms.Padding(0);
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenFile,
+            this.btnSaveFile,
+            this.btnExit,
+            this.toolStripSeparator2,
+            this.btnConvertToCSharp,
+            this.toolStripSeparator1,
+            this.btnConvertToCplusplus,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(12, 0, 2, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(711, 48);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOpenFile.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenFile.Image")));
+            this.btnOpenFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(34, 43);
+            this.btnOpenFile.Text = "toolStripButton1";
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSaveFile.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveFile.Image")));
+            this.btnSaveFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(34, 43);
+            this.btnSaveFile.Text = "toolStripButton2";
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(34, 43);
+            this.btnExit.Text = "toolStripButton3";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 48);
+            // 
+            // btnConvertToCSharp
+            // 
+            this.btnConvertToCSharp.AutoSize = false;
+            this.btnConvertToCSharp.BackColor = System.Drawing.Color.LightBlue;
+            this.btnConvertToCSharp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnConvertToCSharp.Image = ((System.Drawing.Image)(resources.GetObject("btnConvertToCSharp.Image")));
+            this.btnConvertToCSharp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConvertToCSharp.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.btnConvertToCSharp.Name = "btnConvertToCSharp";
+            this.btnConvertToCSharp.Size = new System.Drawing.Size(38, 32);
+            this.btnConvertToCSharp.Text = "C#";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 48);
+            // 
+            // btnConvertToCplusplus
+            // 
+            this.btnConvertToCplusplus.AutoSize = false;
+            this.btnConvertToCplusplus.BackColor = System.Drawing.Color.LightBlue;
+            this.btnConvertToCplusplus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnConvertToCplusplus.Image = ((System.Drawing.Image)(resources.GetObject("btnConvertToCplusplus.Image")));
+            this.btnConvertToCplusplus.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConvertToCplusplus.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.btnConvertToCplusplus.Name = "btnConvertToCplusplus";
+            this.btnConvertToCplusplus.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.btnConvertToCplusplus.Size = new System.Drawing.Size(51, 32);
+            this.btnConvertToCplusplus.Text = "C++";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 43);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.outputTextbox.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextbox.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputTextbox.Location = new System.Drawing.Point(0, -3);
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.Size = new System.Drawing.Size(288, 379);
+            this.outputTextbox.TabIndex = 3;
+            this.outputTextbox.Text = "";
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.inputTextbox.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextbox.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.inputTextbox.Location = new System.Drawing.Point(-2, -3);
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(395, 382);
+            this.inputTextbox.TabIndex = 2;
+            this.inputTextbox.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 81);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 190, 3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.outputTextbox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.inputTextbox);
+            this.splitContainer1.Size = new System.Drawing.Size(689, 375);
+            this.splitContainer1.SplitterDistance = 285;
+            this.splitContainer1.SplitterIncrement = 2;
+            this.splitContainer1.SplitterWidth = 11;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // TbFileName
+            // 
+            this.TbFileName.Location = new System.Drawing.Point(13, 52);
+            this.TbFileName.Name = "TbFileName";
+            this.TbFileName.Size = new System.Drawing.Size(100, 27);
+            this.TbFileName.TabIndex = 7;
+            this.TbFileName.Text = "file1.txt";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(624, 42);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 33);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(545, 42);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 33);
+            this.btnCopy.TabIndex = 9;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 468);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.TbFileName);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnSaveFile;
+        private System.Windows.Forms.ToolStripButton btnOpenFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnConvertToCSharp;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnConvertToCplusplus;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.RichTextBox outputTextbox;
+        private System.Windows.Forms.RichTextBox inputTextbox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TextBox TbFileName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
